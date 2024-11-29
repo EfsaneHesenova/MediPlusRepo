abcb4a8c5c04342f1b197cae9685b4f7bf7a6171
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-11-29 12:48:45 +0400

    MediPlus Front to back

---
+++ MediplusMVC/MediplusMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MediplusMVC.DAL;
+using MediplusMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediplusMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<SliderItem> sliderItems = await _context.SliderItems.OrderByDescending(s=> s.Id).Take(3).ToListAsync();
+            return View(sliderItems);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MediplusMVC/MediplusMVC/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MediplusMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediplusMVC.DAL
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+		public DbSet<SliderItem> SliderItems { get; set; }
+	}
+}
