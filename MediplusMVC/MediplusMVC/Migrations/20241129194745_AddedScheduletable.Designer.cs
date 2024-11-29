132f8471c714e22d8ff2eedbd4a6ab25a8ce4c38
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-11-29 23:50:33 +0400

    MedPlus SChedule dynamic

--- MediplusMVC/MediplusMVC/Migrations/20241129194745_AddedScheduletable.Designer.cs
+++ MediplusMVC/MediplusMVC/Migrations/20241129194745_AddedScheduletable.Designer.cs
@@ -4,6 +4,7 @@ using MediplusMVC.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MediplusMVC.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241129194745_AddedScheduletable")]
+    partial class AddedScheduletable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
